1ee28ad8fc7cf3dee57a8892d49fff2de9ecfe2d
catherinesofio <catalinasofioavogadro@hotmail.com>
2019-07-10 23:57:57 -0300

    Firebase Implementation

--- nysl-app/src/views/Event.vue
+++ nysl-app/src/views/Event.vue
@@ -1,19 +1,25 @@
 <template>
   <section>
-    <div class="double-container">
-      <p class="i">{{ getIcon() }}</p>
-      <p>{{ getEvent().description }}</p>
+    <div class="container-m">
+      <div class="container-4">
+        <div class="container-title">
+          <h2>{{ getMascotNav }}</h2>
+        </div>
+        <p>{{ getEvent().description }}</p>
+      </div>
     </div>
-    <div class="double-container">
-      <ul>
-        <li>{{ getDate }}</li>
-        <li>{{ getTime }}</li>
-        <li>
-          <button class="btn-map" v-on:click="showMap()">🗺️</button>
-          <p>{{ getLocationTitle }}</p>
-        </li>
+    <div class="container-transition">
+      <button class="btn-map-e" v-on:click="showMap()">🗺️</button>
+      <h2>{{ getLocationTitle }}</h2>
+    </div>
+    <div class="container-s">
+      <div class="container-3">
+        <img class="fit" v-bind:src="getLocationImg" v-bind:alt="getLocationAddress" />
+      </div>
+      <ul class="container-3">
+        <li><p>{{ getDate }}</p></li>
+        <li><p>{{ getTime }}</p></li>
       </ul>
-      <img v-bind:src="getLocationImg" />
     </div>
     <Map v-bind:url="getLocationMap" />
   </section>
@@ -26,20 +32,21 @@ import store from "@/store.js";
 
 export default {
   name: "event",
-  data: function () {
+  data: function() {
     return {
-      icons: ["⚽", "🏆"]
-    }
+      types: ["friendly match", "championship"],
+      icons: [ "🕒", "📅"]
+    };
   },
   components: {
     Map
   },
   methods: {
-    getEvent: function () {
+    getEvent: function() {
       return this.eventData.event;
     },
-    getIcon: function () {
-      return this.icons[this.getEvent().icon];
+    getType: function() {
+      return this.types[this.getEvent().icon];
     },
     showMap: function(url) {
       document.getElementById("map").style.display = "block";
@@ -53,12 +60,15 @@ export default {
 
       return new Date(date.year, date.month, date.day, time.hour, time.minutes);
     },
+    getTeamMascot(id) {
+      return this.teams[id].mascot;
+    },
     showMap: function() {
       document.getElementById("map").style.display = "block";
     }
   },
   computed: {
-    ...mapState(["games", "locations", "eventData"]),
+    ...mapState(["games", "teams", "locations", "eventData"]),
     getLocationTitle() {
       return this.getLocation().title;
     },
@@ -68,20 +78,32 @@ export default {
     getLocationMap() {
       return this.getLocation().map_url;
     },
+    getLocationAddress() {
+      return this.getLocation().address;
+    },
     getDate() {
       let date = this.getDatetime();
 
-      return date.toLocaleDateString();
+      return this.icons[1] + " " + date.toLocaleDateString();
     },
     getTime() {
       let date = this.getDatetime();
 
-      return date.toLocaleTimeString();
+      return this.icons[0] + " " + date.toLocaleTimeString();
+    },
+    getMascotNav() {
+      let event = this.getEvent();
+
+      return (
+        this.getTeamMascot(event.team_a_id) +
+        " vs " +
+        this.getTeamMascot(event.team_b_id)
+      );
     }
   },
   mounted() {
     store.dispatch("setNavData", {
-      title: this.eventData.title,
+      title: this.getType(),
       path: "/games",
       showPath: true
     });
@@ -90,51 +112,130 @@ export default {
 </script>
 
 <style>
-.i {
-  margin: 0;
-  padding: 0;
-  font-size: 25vw;
-
-  text-shadow: none;
-}
-
-.double-container {
+.container-m, .container-s {
   display: flex;
   flex-wrap: nowrap;
 
-  width: 100%;
+  width: 100vw;
+  height: 45vh;
 
   background-color: #202020;
   box-shadow: var(--shadow);
 
   overflow: hidden;
+
+  z-index: var(--middle-layer);
 }
 
-.img {
-  width: 50vw;
-
-  z-index: var(--back-layer);
+.container-s {
+  height: 35vh;
 }
 
-.double-container p,
-.double-container ul {
-  width: 100%;
-  height: 50vmax;
+.container-1,
+.container-2,
+.container-3,
+.container-4 {
+  height: 100%;
 
-  background-color: var(--title-color);
+  padding: 1em;
+
+  text-align: justify;
+
+  background-color: whitesmoke;
+
+  overflow: hidden;
 
   z-index: var(--top-layer);
 }
 
-.double-container li {
-  display: flex;
+.container-1 p,
+.container-2 p,
+.container-3 p,
+.container-4 p {
+  font-size: 1.5em;
+
+  text-align: justify;
 }
 
-.btn-map {
+.container-1 {
+  width: 25%;
+}
+
+.container-2 {
+  width: 75%;
+}
+
+.container-3 {
+  width: 50%;
+}
+
+.container-4 {
+  width: 100%;
+}
+
+.container-1 li,
+.container-2 li {
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: row;
+}
+
+.container-1 img,
+.container-2 img {
+  height: 100%;
+
+  filter: saturate(125%);
+}
+
+.container-title {
+  display: flex;
+  flex-wrap: nowrap;
+
+  width: 100%;
+
+  margin-bottom: 1em;
+
+  background-image: linear-gradient(
+    to bottom right,
+    var(--sec-color),
+    var(--main-color)
+  );
+  background-repeat: no-repeat;
+
+  text-align: center;
+}
+
+.container-title h2 {
+  width: 100%;
+
+  padding: 0.5em;
+
+  text-align: center;
+}
+
+.container-transition {
+  display: flex;
+  flex-wrap: nowrap;
+
+  padding: 1em;
+
+  width: 100%;
+
+  padding-bottom: 1em;
+
+  background-color: var(--third-color);
+  background-image: linear-gradient(to right, var(--third-color), #050505);
+
+  text-align: left;
+}
+
+.container-transition .btn-map-e {
+  margin-right: 0.5em;
+}
+
+.btn-map-e {
   width: 2em;
   height: 2em;
-
-  margin: 0.5em;
 
   font-size: 1em;
 
@@ -156,5 +257,29 @@ export default {
   border-color: var(--sec-color);
 
   z-index: var(--top-layer);
+}
+
+.fit {
+  height: 100%;
+
+  object-fit: scale-down;
+}
+
+li {
+  list-style: none;
+}
+
+.container-m p {
+  font-size: 1.5em;
+  text-shadow: none;
+}
+
+.container-s p {
+  font-size: 1.25em;
+  text-shadow: none;
+}
+
+.container-transition h2 {
+  text-align: left;
 }
 </style>
--- nysl-app/src/views/Teams.vue
+++ nysl-app/src/views/Teams.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="info-container" v-for="t in teams" v-bind:key="t.id">
       <img v-bind:src="t.img_url" />
-      <h2>{{ t.title }}</h2>
+      <h2>{{ getTitle(t) }}</h2>
     </div>
   </section>
 </template>
@@ -13,7 +13,14 @@ import store from "@/store.js";
 
 export default {
   name: "teams",
-  computed: mapState(["teams"]),
+  computed: {
+    ...mapState(["teams"]),
+  },
+  methods: {
+    getTitle: function (t) {
+      return (t.id % 2 == 0) ? t.mascot + " " + t.title: t.title + " " + t.mascot;
+    }
+  },
   mounted() {
     store.dispatch("setNavData", { title: "TEAMS", path: "/", showPath: true });
   }
